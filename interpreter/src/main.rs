a1798e24d5dea4081b9726b002a030eebe65885d
Ian Melendez <ian.melendez.95@utexas.edu>
2021-07-24 19:23:35 -0500

    first stack code parser implementation

--- interpreter/src/main.rs
+++ interpreter/src/main.rs
@@ -6,9 +6,5 @@ use parser::LExpr::*;
 const TEXT: &str = "true";
 
 fn main() {
-  let (result, _): (LExpr, &str) = parse_bool(TEXT).unwrap();
-  match result {
-    LBool(b) => println!("Got boolean!: {}", b),
-    _ => panic!("didn't parse a boolean")
-  }
+  panic!();
 }
--- interpreter/src/parser/mod.rs
+++ interpreter/src/parser/mod.rs
@@ -1,3 +1,5 @@
+use LExpr::*;
+use PElem::*;
 
 pub enum LExpr {
   // constants
@@ -8,40 +10,91 @@ pub enum LExpr {
   // variable
   LVar(String),
 
-
+  // abstractions
   LApp(Box<LExpr>, Box<LExpr>),
-  LLambda(String, Box<LExpr>)
+  LLambda(String, Box<LExpr>),
+
+  // enriched bindings
+  LLet(LBind, Box<LExpr>),
+  LLetrec(Vec<LBind>, Box<LExpr>)
 }
 
-pub type ParseResult<'a,T> = Result<(T, &'a str), String>;
+pub struct LBind {
+  var: String,
+  body: Box<LExpr>
+}
 
-pub fn parse(input: &str) {
+/// Parse Element
+///
+/// Represents elements as parsed blindly
+/// in the stack parser.
+///
+/// Intended to be validated and finally
+/// coerced into the final LExpr
+pub enum PElem {
+  PVar(String),
+
+  PNat(u32),
+  PString(String),
+  PBool(bool),
+
+  PApp(Box<PElem>, Box<PElem>),
+  PLambda(Box<PElem>, Box<PElem>),
+
+  PBind(Box<PElem>, Box<PElem>), // (var, expr)
+  PLet(Box<PElem>, Box<PElem>),  // (binding, body)
+  PLrec(Vec<PElem>, Box<PElem>), // (bindings, body)
+}
+
+pub fn parse_stack_code(input: &str) {
   if !input.is_ascii() { panic!("Can only parse ASCII text!") }
 
-  next_token(input);
+  let mut stack: Vec<PElem> = Vec::new();
+  for line in input.lines() {
+    if line.starts_with("VAR") {
+      stack.push(PVar(String::from(&line[4..])));
+    } else if line.starts_with("CON") {
+      stack.push(parse_const(&line[6..]));
+    } else if line == "APP" {
+      let pop1 = stack.pop().unwrap();
+      let pop2 = stack.pop().unwrap();
+      stack.push(PApp(Box::from(pop1), Box::from(pop2)));
+    } else if line == "LAM" {
+      let pop1 = stack.pop().unwrap();
+      let pop2 = stack.pop().unwrap();
+      stack.push(PLambda(Box::from(pop1), Box::from(pop2)));
+    } else if line == "BIN" {
+      let pop1 = stack.pop().unwrap();
+      let pop2 = stack.pop().unwrap();
+      stack.push(PBind(Box::from(pop1), Box::from(pop2)));
+    } else if line == "LET" {
+      let pop1 = stack.pop().unwrap();
+      let pop2 = stack.pop().unwrap();
+      stack.push(PLet(Box::from(pop1), Box::from(pop2)));
+    } else if line == "LRE" {
+      // pop the bindings
+      let bindings: Vec<PElem>; {
+        let mut bs_temp: Vec<PElem> = Vec::new();
+        let binding_count = line[4..].parse::<usize>().unwrap();
+        for _ in 0..binding_count {
+          bs_temp.push(stack.pop().unwrap());
+        }
+        bindings = bs_temp;
+      }
+
+      // pop the body
+      let pop2 = stack.pop().unwrap();
+
+      // push the recursive let
+      stack.push(PLrec(bindings, Box::from(pop2)));
+    }
+  }
+
   return
 }
 
-fn parse_expr<'a>(input: &'a str) -> ParseResult<'a, LExpr> {
-  if input.starts_with("true") || input.starts_with("false") {
-    parse_bool(input)
-  } else {
-    panic!("Cannot parse expression: {}", input)
-  }
-}
-
-fn next_token<'a>(input: &'a str) -> ParseResult<'a, &str> {
-  return Ok(("", input));
-}
-
-pub fn parse_bool<'a>(input: &'a str) -> ParseResult<'a, LExpr> {
-  if input.starts_with("true") {
-    Ok((LExpr::LBool(true), &input[4..]))
-  } else if input.starts_with("false") {
-    Ok((LExpr::LBool(false), &input[5..]))
-  } else {
-    panic!("Cannot parse boolean: {}", input)
-  }
+fn parse_const(input: &str) -> PElem {
+  panic!();
 }
 
 #[cfg(test)]
