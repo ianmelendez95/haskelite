e60cc4c807071d4b6b9d6cd08f994753597e627f
Ian Melendez <ian.melendez.95@utexas.edu>
2021-08-25 22:18:48 -0500

    supercombinator compiler WIP

--- compiler/src/Lambda/SCCompiler.hs
+++ compiler/src/Lambda/SCCompiler.hs
@@ -2,76 +2,87 @@
 
 module Lambda.SCCompiler where
 
-import Control.Monad.State.Lazy as ST
+import Control.Exception (assert)
+import qualified Control.Monad.State.Lazy as ST
+
 import qualified Lambda.Syntax as S
 
 
-data SC = SC String [String] S.Exp
-
 data Prog = Prog [SC] S.Exp
 
-data SCEnv = SCEnv {
-  scenvSCCount :: Integer,
-  scenvCombs :: [SC],
-  scenvBVars :: [(Int, String)],
-  scenvFVars :: [(Int, [String])]
-}
+data SC = SC String [String] S.Exp
+
 
 type SCS = ST.State SCEnv
 
+data SCEnv = SCEnv {
+  scenvSCCount :: Integer,
+  scenvCombs :: [SC]
+}
+
 emptySCEnv :: SCEnv 
-emptySCEnv = SCEnv { scenvSCCount = 0, scenvCombs = [], scenvBVars = [], scenvFVars = [] }
+emptySCEnv = SCEnv { scenvSCCount = 0, scenvCombs = [] }
 
 newSCName :: SCS String
 newSCName = 
-  do modify (\scenv@SCEnv{ scenvSCCount = c } -> scenv{ scenvSCCount = c+1 })
-     cur_count <- gets scenvSCCount
+  do ST.modify (\scenv@SCEnv{ scenvSCCount = c } -> scenv{ scenvSCCount = c+1 })
+     cur_count <- ST.gets scenvSCCount
      pure $ '$' : show cur_count
 
 pushSC :: SC -> SCS ()
 pushSC sc = ST.modify (\scenv@SCEnv{ scenvCombs = scs } -> 
   scenv{ scenvCombs = sc : scs })
 
-pushBV :: String -> SCS ()
-pushBV var = ST.modify modEnv
-  where
-    modEnv :: SCEnv -> SCEnv 
-    modEnv scenv@SCEnv{ scenvBVars = bvars } = 
-      scenv{ scenvBVars = modBVars bvars }
-    
-    modBVars :: [(Int, String)] -> [(Int, String)]
-    modBVars [] = [(1, var)]
-    modBVars bvars@((n, _):_) = (n + 1, var) : bvars
-
 
 compileSCs :: S.Exp -> Prog
 compileSCs expr = 
-  let (expr', SCEnv{ scenvCombs = scs }) = ST.runState (csc expr) emptySCEnv
+  let (expr', SCEnv{ scenvCombs = scs }) = 
+        ST.runState (csc [] expr) emptySCEnv
    in Prog scs expr'
 
 
-csc :: S.Exp -> SCS S.Exp
-csc t@(S.Term _) = pure t 
-csc (S.Apply e1 e2 ) = S.Apply <$> csc e1 <*> csc e2
-csc (S.Let (var, val) body) = 
-  do val'  <- csc val
-     body' <- csc body
+data SCCtx = SCCtx {
+  scctxBVs :: [(Int, String)],
+  scctxFVs :: [String]
+}
+
+
+csc :: [String] -> S.Exp -> SCS S.Exp
+csc _ t@(S.Term _) = pure t 
+csc bound_vars (S.Apply e1 e2 ) = S.Apply <$> csc bound_vars e1 <*> csc bound_vars e2
+csc bound_vars (S.Let (var, val) body) = 
+  do val'  <- csc bound_vars val
+     body' <- csc (var : bound_vars) body
      pure $ S.Let (var, val') body'
-csc (S.Letrec bs e)  = 
-  do bs' <- mapM (\(var, val) -> (var,) <$> csc val) bs
-     e'  <- csc e
+csc bound_vars (S.Letrec bs e)  = 
+  do let bound_vars' = map fst bs ++ bound_vars
+     bs' <- mapM (\(var, val) -> (var,) <$> csc bound_vars' val) bs
+     e'  <- csc bound_vars' e
      pure $ S.Letrec bs' e'
-csc (S.Lambda v b)   = 
+csc bound_vars (S.Lambda var body)   = 
   do sc_name <- newSCName
 
-     pushBV v
-     b' <- csc b
+     body' <- csc (var : bound_vars) body
 
-     let sc_vars = undefined
+     -- So we want the free variables within the body,
+     -- but in their 'bound order' (their De Brujin index)
+     --
+     -- Since we have the bound vars in ascending order (bound_vars)
+     -- we can just filter that list for our free variables!
+     let body_vars = S.freeVariables' [var] body'
+         sc_free_vars = filter (`elem` body_vars) bound_vars
+         sc_vars = var : 
+           assert (length body_vars == length sc_free_vars) sc_free_vars
 
-     pushSC (SC sc_name sc_vars b')
+     -- add the supercombinator to the collection
+     pushSC (SC sc_name sc_vars body')
 
+     -- return the expression that uses the supercombinator
      pure $ S.mkApply (S.mkVariable sc_name : map S.mkVariable sc_vars)
 
-
+  
+isSCVar :: String -> Bool
+isSCVar [] = error "Empty variable"
+isSCVar ('$':_) = True
+isSCVar _ = False
 
