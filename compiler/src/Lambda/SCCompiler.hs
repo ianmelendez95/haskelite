59384e2ee03ed6709719e8fb7da6f311585c977f
Ian Melendez <ian.melendez.95@utexas.edu>
2021-09-19 21:40:48 -0500

    incremental supercomb naming

--- compiler/src/Lambda/SCCompiler.hs
+++ compiler/src/Lambda/SCCompiler.hs
@@ -6,7 +6,7 @@ module Lambda.SCCompiler
   , compileExpr
   ) where
 
-import Data.List (foldl1', (\\), intersect, union)
+import Data.List ((\\), intersect, union)
 import qualified Control.Monad.State.Lazy as ST
 import qualified Lambda.Syntax as S
 
@@ -17,14 +17,18 @@ traceMsg _ x = x
 
 data Prog = Prog [SC] S.Exp
 
--- traverseProgs :: [Prog] -> ([SC], [S.Exp])
--- traverseProgs = mconcat . map (\(Prog scs expr) -> (scs, [expr]))
+instance Show Prog where
+  show (Prog scs expr) = unlines (map show scs ++ [replicate 80 '-', show expr])
 
-appendProgs :: (S.Exp -> S.Exp -> S.Exp) -> Prog -> Prog -> Prog
-appendProgs ef (Prog scs1 e1) (Prog scs2 e2) = Prog (scs1 ++ scs2) (ef e1 e2)
 
--- mapProg :: (S.Exp -> S.Exp) -> Prog -> Prog
--- mapProg f (Prog scs e) = Prog scs (f e)
+data SC = SC {
+  scName :: String,
+  scParams :: [String],
+  scBody :: S.Exp
+}
+
+instance Show SC where
+  show (SC name args expr) = unwords (name : args) ++ " = " ++ show expr
 
 
 data SuperComb = Let (String, SuperComb) SuperComb
@@ -37,37 +41,38 @@ data SuperComb = Let (String, SuperComb) SuperComb
                | NComb [String] [String] SuperComb
                deriving Show
 
-data SC = SC {
-  scName :: String,
-  scParams :: [String],
-  scBody :: S.Exp
-}
-
-instance Show Prog where
-  show (Prog scs expr) = unlines (map show scs ++ [replicate 80 '-', show expr])
-
-instance Show SC where
-  show (SC name args expr) = unwords (name : args) ++ " = " ++ show expr
 
 type SCS = ST.State SCEnv
+
+scsToProg :: SCS S.Exp -> Prog
+scsToProg scstate = 
+  let (body, SCEnv{ scenvCombs = scs }) = ST.runState scstate emptySCEnv
+   in Prog scs body
+
+newSCName :: SCS String
+newSCName = ('$':) . show <$> new_count
+  where 
+    new_count = 
+      do ST.modify (\scenv@SCEnv{ scenvSCCount = c } -> scenv{ scenvSCCount = c+1 })
+         ST.gets scenvSCCount
+
+pushSC :: SC -> SCS ()
+pushSC sc = ST.modify (scenvPushSC sc)
+
 
 data SCEnv = SCEnv {
   scenvSCCount :: Integer,
   scenvCombs :: [SC]
 }
 
-
-mkApp :: [SuperComb] -> SuperComb
-mkApp = foldl1' App
+scenvPushSC :: SC -> SCEnv -> SCEnv
+scenvPushSC sc scenv@SCEnv{ scenvCombs = scs } = scenv{ scenvCombs = sc : scs }
 
 emptySCEnv :: SCEnv
 emptySCEnv = SCEnv { scenvSCCount = 0, scenvCombs = [] }
 
-newSCName :: SCS String
-newSCName =
-  do ST.modify (\scenv@SCEnv{ scenvSCCount = c } -> scenv{ scenvSCCount = c+1 })
-     cur_count <- ST.gets scenvSCCount
-     pure $ '$' : show cur_count
+
+type Bind a = (String, a)
 
 
 --------------------------------------------------------------------------------
@@ -170,41 +175,36 @@ resolveFreeParams bound (NComb bound_ps free_ps body) =
 
 
 liftSuperCombs :: SuperComb -> Prog
-liftSuperCombs (Let (bvar, bval) body) = 
-  appendProgs (\bval' body' -> S.Let (bvar, bval') body') 
-              (liftSuperCombs bval) 
-              (liftSuperCombs body)
+liftSuperCombs comb = scsToProg (liftSuperCombsM comb)
+  
+liftSuperCombsM :: SuperComb -> SCS S.Exp
+liftSuperCombsM (Let bind body) = 
+  S.Let <$> mapM liftSuperCombsM bind
+        <*> liftSuperCombsM body
 
-liftSuperCombs (Letrec binds body) = 
-  let (binds_scs, binds') = liftBindings binds
-      (Prog body_scs body') = liftSuperCombs body
-   in Prog (binds_scs ++ body_scs) (S.Letrec binds' body')
+liftSuperCombsM (Letrec binds body) = 
+  S.Letrec <$> liftBindings binds
+           <*> liftSuperCombsM body
   where
-    liftBindings :: [(String, SuperComb)] -> ([SC], [(String, S.Exp)])
-    liftBindings = mconcat . map liftBinding
+    liftBindings :: [Bind SuperComb] -> SCS [Bind S.Exp]
+    liftBindings = traverse liftBinding
 
-    -- 'lift' the binding, retrieving the supercombinators
-    -- and the new binding
-    --
-    -- returns the binding as a singleton list so it can be 
-    -- simply concatenated
-    liftBinding :: (String, SuperComb) -> ([SC], [(String, S.Exp)])
-    liftBinding (var, val) = 
-      let (Prog scs val') = liftSuperCombs val
-        in (scs, [(var, val')])
+    liftBinding :: Bind SuperComb -> SCS (Bind S.Exp)
+    liftBinding bind = mapM liftSuperCombsM bind
   
 -- TODO: consider renaming free vars that match named supercombs
-liftSuperCombs (Term t) = Prog [] (S.Term t)
-liftSuperCombs (App sc1 sc2) = 
-  appendProgs S.Apply (liftSuperCombs sc1) (liftSuperCombs sc2)
-liftSuperCombs (NComb bound_ps free_ps body) = 
-  let (Prog b_scs body') = liftSuperCombs body
-      new_sc = SC {
-        scName = "TODO GEN NAME",
-        scParams = bound_ps ++ free_ps,
-        scBody = body'
-      }
-   in Prog (new_sc : b_scs) (S.mkApply . map S.mkVariable $ "TODO GEN NAME" : free_ps)
+liftSuperCombsM (Term t) = pure $ S.Term t
+liftSuperCombsM (App sc1 sc2) = 
+  S.Apply <$> liftSuperCombsM sc1 <*> liftSuperCombsM sc2
+liftSuperCombsM (NComb bound_ps free_ps body) = 
+  do sc_name <- newSCName
+     body'   <- liftSuperCombsM body
+     pushSC $ SC{
+       scName = sc_name,
+       scParams = bound_ps ++ free_ps,
+       scBody = body'
+     }
+     pure $ S.mkApply (map S.mkVariable (sc_name : free_ps))
 
 
 -- | Identifies directly nested supercombinators,
