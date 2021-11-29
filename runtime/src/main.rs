f39c317c3980618a759e03a15024035117ae9dd2
Ian Melendez <ian.melendez.95@utexas.edu>
2021-11-29 12:54:40 -0600

    [runtime] initial types

--- runtime/src/main.rs
+++ runtime/src/main.rs
@@ -1,3 +1,42 @@
+use std::collections::HashMap;
+
+mod builtins;
+
+pub type UBInt = u64;
+
+pub enum GCode {
+
+}
+
+pub enum GNode {
+  GInt(UBInt),
+  GCons(Box<GNode>, Box<GNode>),
+  GAp(Box<GNode>, Box<GNode>),
+  GFun(u64, Box<Prog>),
+  GHole()
+}
+
+pub struct DNode {
+  pub stack: Stack,
+  pub prog: Prog
+}
+
+pub type Stack = Vec<GNode>;
+pub type Graph = HashMap<String, GNode>;
+pub type Prog = Vec<GCode>;
+pub type Dump = Vec<DNode>;
+
+pub struct GState {
+  pub stack: Stack,
+  pub graph: Graph,
+  pub prog: Prog,
+  pub dump: Dump
+}
+
 fn main() {
-    println!("Hello, world!");
+  let stack: Stack = panic!();
+  let graph: Graph = panic!();
+  let dump:  Dump = panic!();
+
+
 }
